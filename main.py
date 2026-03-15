5f656aec754d1ea564f8d7b0137d303a82dc3638
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2026-03-15 17:29:12 -0300

    data preprocessing implemented

---
+++ main.py
@@ -0,0 +1,34 @@
+# Data preprocessing
+def readFile():
+    fileName = open("MushroomData_8000.txt")
+    info = []
+    for lines in fileName:
+        info.append(lines.rstrip('\n'))
+    splitData(info)
+
+def splitData(data):
+    train = []
+    test = []
+    answer = []
+    check = 1 # to facilitate the split of data. A value of 1 gives training = 5600 and testing = 2400
+    trainSplit = 0.7 # 5600 lines of data will be kept for training
+    trainAmount = len(data) * trainSplit #
+    #gotta check if the conversion to numbers should happen HERE or in KNN. I feel like it should be here
+    for i in data:
+        b = list(map(ord, i)) #ord returns the number representing the unicode code of a specified character, my only concern is how this might affect the '?' character
+        # print(b)
+        if check <= trainAmount:
+            train.append(b)
+        else:
+            answer.append(b[0])
+            test.append(b[1:])
+        check = check+1
+    print(len(train), " ", len(test), " ", answer)
+    return train, test, answer
+
+def main():
+    readFile()
+
+
+if __name__ == "__main__":
+    main()
