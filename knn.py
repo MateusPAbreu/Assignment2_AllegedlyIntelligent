303384c969702b220da3bd3c600057b927710e18
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2026-03-23 19:02:11 -0300

    knn concluded

--- knn.py
+++ knn.py
@@ -1,21 +1,5 @@
 import math
 
-def knn(data, k):
-    for i in range(len(data)): 
-        main = data[i]
-        # FIXES THIS NEEDS: 
-        # Right now it breaks because, when it reaches the last element, I think I should go back to look at the neighbors above.
-        # at the moment I'm only considering the items below the current item, have to change that to consider those above it too.
-        for j in range(k):
-            if i+j >= len(data):
-                #do something
-                nearest = data[i+1]
-            else:
-                nearest = data[i+j]
-            print(i, j, len(data))
-            #should probably get the result, compare what's the highest and then determine if it's poisonous or edible based on that
-        # print(main, nearest)
-
 def KClosest(element, data, k):
     #Initializing
     KClosestIndex = []
@@ -33,30 +17,31 @@ def KClosest(element, data, k):
             KClosestIndex.append(i)
             KClosestDistance.append(calc(element, data[i]))
             foundItFlag = False
+        
+    return data, KClosestIndex, k
 
-    scoreTracker = 0
 
-    for i in range(k):
-        curMush = data[KClosestIndex[i]]
-        if(curMush[0]==101): #Compair to the ascii code for 'e'
-            scoreTracker += 1
+def checkNeighbors(data, closestData, k):
+    # I'm making two lists, for the two different classes, and inserting the first value on the list of closest neighbors, into the first class (arbitrary choice)
+    # In the end, what matters is which list is larger
+    classOne = [] 
+    classTwo = []
+    classOne.append(data[closestData[0]][0])
+
+    for i in range(1, k):
+        print("Class One ", classOne, " Class Two ", classTwo)
+
+        if classOne[0] == data[closestData[i]][0]:
+            classOne.append(data[closestData[i]][0])
         else:
-            scoreTracker -= 1
-        print(KClosestIndex[i], "\n")
-        print(data[KClosestIndex[i]])
-
-    if(scoreTracker>0):
-        print("The sample is edible")
-        return 1
-    elif(scoreTracker==0):
-        print("Inconclusive")
-        return 0
+            classTwo.append(data[closestData[i]][0])
+    
+    if(len(classOne) > len(classTwo)):
+        return chr(classOne[0])
     else:
-        print("The sample is not edible")
-        return -1
-
+        return chr(classTwo[0])
 
 
 def calc(main, nearest):
-    dist = math.sqrt((main[1] - nearest[1])**2 + (main[2] - nearest[2])**2 + (main[3] - nearest[3])**2 + (main[4] - nearest[4])**2 + (main[5] - nearest[5])**2 + (main[6] - nearest[6])**2 + (main[7] - nearest[7])**2 + (main[8] - nearest[8])**2 + (main[9] - nearest[9])**2 + (main[10] - nearest[10])**2 + (main[11] - nearest[11])**2 + (main[12] - nearest[12])**2 + (main[13] - nearest[13])**2 + (main[14] - nearest[14])**2 + (main[15] - nearest[15])**2 + (main[16] - nearest[16])**2 + (main[17] - nearest[17])**2 + (main[18] - nearest[18])**2 + (main[19] - nearest[19])**2 + (main[20] - nearest[20])**2 + (main[21] - nearest[21])**2 + (main[22] - nearest[22])**2)
+    dist = math.sqrt((main[0] - nearest[1])**2 + (main[1] - nearest[2])**2 + (main[2] - nearest[3])**2 + (main[3] - nearest[4])**2 + (main[4] - nearest[5])**2 + (main[5] - nearest[6])**2 + (main[6] - nearest[7])**2 + (main[7] - nearest[8])**2 + (main[8] - nearest[9])**2 + (main[9] - nearest[10])**2 + (main[10] - nearest[11])**2 + (main[11] - nearest[10])**2 + (main[12] - nearest[13])**2 + (main[13] - nearest[14])**2 + (main[14] - nearest[15])**2 + (main[15] - nearest[16])**2 + (main[16] - nearest[17])**2 + (main[17] - nearest[18])**2 + (main[18] - nearest[19])**2 + (main[20] - nearest[19])**2 + (main[21] - nearest[20])**2 + (main[21] - nearest[22])**2)
     return dist
--- main.py
+++ main.py
@@ -5,8 +5,18 @@ def readFile():
     fileName = open("MushroomData_8000.txt")
     info = []
     for lines in fileName:
-        info.append(lines.rstrip(',\n'))
-    splitData(info)
+        info.append(lines.rstrip('\n').replace(",",""))
+    return info
+
+def readUnknownData():
+    fileName = open("MushroomData_Unknwon_100.txt")
+    info = []
+    unknownData = []
+    for lines in fileName:
+        info.append(lines.rstrip('\n').replace(",",""))
+    for i in info:
+        unknownData.append(list(map(ord, i)))
+    return unknownData
 
 def splitData(data):
     train = []
@@ -15,24 +25,27 @@ def splitData(data):
     check = 1 # to facilitate the split of data. A value of 1 gives training = 5600 and testing = 2400
     trainSplit = 0.7 # 5600 lines of data will be kept for training
     trainAmount = len(data) * trainSplit #
-    #gotta check if the conversion to numbers should happen HERE or in KNN. I feel like it should be here
     for i in data:
         b = list(map(ord, i)) #ord returns the number representing the unicode code of a specified character, my only concern is how this might affect the '?' character
-        # print(b)
         if check <= trainAmount:
             train.append(b)
         else:
             answer.append(b[0])
             test.append(b[1:])
         check = check+1
-    c = train[1]
-    # print(c[1])
-    #knn.knn(train, 5)
-    knn.KClosest(train[157], train, 5)
+
     return train, test, answer
 
 def main():
-    readFile()
+    attributeClass = []
+    info = readFile()
+    unknownData = readUnknownData()
+    train, test, answer= splitData(info)
+    f = open("predictionResultKNN.txt", "w")
+    for i in range(len(unknownData)):
+        data, KClosestIndex, k = knn.KClosest(unknownData[i], train, 10)
+        print(knn.checkNeighbors(data, KClosestIndex, k))
+        f.write(knn.checkNeighbors(data, KClosestIndex, k) +'\n')
 
 
 if __name__ == "__main__":
